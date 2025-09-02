49631b32f0c415bcd6d9ef152ab0e2ffe9e8c8ca
Jakub Jaskulski <jja@unatrix.com>
2025-09-02 09:38:03 +0200

    Remove unused vars

--- variables.tf
+++ variables.tf
@@ -1,29 +1,11 @@
-variable "application_name" {
-  description = "Name of the Azure AD application"
-  type        = string
-  default     = "ms-callback-app"
-}
-
 variable "redirect_uri" {
   description = "Redirect URI for OAuth callback"
   type        = string
   default     = "http://localhost:3000/callback"
 }
 
-variable "environment" {
-  description = "Environment name (dev, staging, prod)"
-  type        = string
-  default     = "dev"
-}
-
 variable "owners" {
   description = "List of object IDs of application owners"
-  type        = list(string)
-  default     = []
-}
-
-variable "additional_redirect_uris" {
-  description = "Additional redirect URIs beyond the default"
   type        = list(string)
   default     = []
 }
